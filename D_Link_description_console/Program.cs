93b9f35644cd46a3abeb3b8ee421dfc20e0710da
Михаил Моисеенко <cray888@gmail.com>
2017-09-21 11:23:18 +0500

    Добавлено получение ошибок по портам

--- D_Link_description_console/Program.cs
+++ D_Link_description_console/Program.cs
@@ -40,26 +40,39 @@ namespace D_Link_description_console
 
             Thread t;
 
-            DescrReciver cliobj = new DescrReciver(input);
+            DescrReciver cliobj = new DescrReciver(input, command);
             t = new Thread(new ThreadStart(cliobj.Run));
             t.Start();
 
             output.Write(login + "\r\n");
             output.Write(password + "\r\n");
 
-            if (endPort != 0)
-            {
-                for (int i = startPort; i < endPort; i = i + 5)
+            if (command == "desc")
+            { 
+                if (endPort != 0 && startPort != endPort)
                 {
-                    output.Write("show ports " + i.ToString() + "-" + ((i + 4) > endPort ? endPort : i + 4).ToString() + " description\r\n");
+                    for (int i = startPort; i < endPort; i = i + 5)
+                    {
+                        output.Write("show ports " + i.ToString() + "-" + ((i + 4) > endPort ? endPort : i + 4).ToString() + " description\r\n");
+                        output.Write("q");
+                    }
+                }
+                else
+                {
+                    output.Write("show ports " + startPort + " description\r\n");
                     output.Write("q");
                 }
             }
-            else
+            else if  (command == "errs")
             {
-                output.Write("show ports " + startPort + " description\r\n");
-                output.Write("q");
+                if (endPort == 0) endPort = startPort;
+                for (int i = startPort; i <= endPort; i++)
+                {
+                    output.Write("show error ports " + i.ToString() + "\r\n");
+                    output.Write("q");
+                }
             }
+
             output.Write("logout\r\n");
             output.Flush();
 
@@ -78,13 +91,17 @@ namespace D_Link_description_console
         public class DescrReciver
         {
             StreamReader input;
+            string command;
             List<PortDescription> portDescriptionList;
+            List<PortError> portErrorList;
             private bool isDGS;
 
-            public DescrReciver(StreamReader input)
+            public DescrReciver(StreamReader input, string command)
             {
                 this.input = input;
+                this.command = command;
                 portDescriptionList = new List<PortDescription>();
+                portErrorList = new List<PortError>();
             }
 
             public void Run()
@@ -96,6 +113,8 @@ namespace D_Link_description_console
                 string link = string.Empty;
                 string descr = string.Empty;
                 string kv = string.Empty;
+                string errCRCRX = string.Empty;
+                string errCRCTX = string.Empty;
 
                 while ((line = input.ReadLine()) != null)
                 {
@@ -103,57 +122,107 @@ namespace D_Link_description_console
                     {
                         isDGS = true;
                     }
-                    if (stringCounter >= 0) stringCounter++;
-                    if (line.Contains("Enabled")) stringCounter = 0;
-                    if (stringCounter >= 0)
+                    if (stringCounter >= 0) stringCounter++;                    
+                    if (command == "desc")
                     {
-                        line = line.Replace("\u001b[?25l", "");
-
-                        if (stringCounter == 0)
+                        if (line.Contains("Enabled")) stringCounter = 0;
+                        if (stringCounter >= 0)
                         {
-                            if (isDGS)
-                            {
-                                port = line.Substring(0, 6);
-                                link = line.Substring(38, 25);
-                            }
-                            else
-                            {
-                                port = line.Substring(0, 6);
-                                link = line.Substring(42, 25);
-                            }
-                        }
-                        else if (stringCounter == 2)
-                        {
-                            descr = line.Substring(6, 25).ToLower();
-                            if (descr.IndexOf("_kv") != -1)
-                            {
-                                kv = descr.Substring(descr.IndexOf("_kv") + 3, 3).Trim();
-                            }
-                            else if (descr.IndexOf("_") != -1)
-                            {
-                                kv = descr.Substring(descr.IndexOf("_") + 1, 3).Trim();
-                            }
-                            else kv = string.Empty;
-                            int intkv = 0;
-                            if (int.TryParse(kv, out intkv) != true)
-                            {
-                                kv = string.Empty;
-                            }
-                        }
+                            line = line.Replace("\u001b[?25l", "");
 
+                            if (stringCounter == 0)
+                            {
+                                if (isDGS)
+                                {
+                                    port = line.Substring(0, 6);
+                                    link = line.Substring(38, 25);
+                                }
+                                else
+                                {
+                                    port = line.Substring(0, 6);
+                                    link = line.Substring(42, 25);
+                                }
+                            }
+                            else if (stringCounter == 2)
+                            {
+                                descr = line.Substring(6, 25).ToLower();
+                                if (descr.IndexOf("_kv") != -1)
+                                {
+                                    kv = descr.Substring(descr.IndexOf("_kv") + 3, 3).Trim();
+                                }
+                                else if (descr.IndexOf("_") != -1)
+                                {
+                                    kv = descr.Substring(descr.IndexOf("_") + 1, 3).Trim();
+                                }
+                                else kv = string.Empty;
+                                int intkv = 0;
+                                if (int.TryParse(kv, out intkv) != true)
+                                {
+                                    kv = string.Empty;
+                                }
+                            }
+
+                        }
+                        if (stringCounter == 2)
+                        {
+                            Console.WriteLine(port + " " + link + " " + descr + " " + kv);
+                            portDescriptionList.Add(new PortDescription() { port = port.Trim(), link = link.Trim(), descr = descr.Trim(), kv = kv });
+                            stringCounter = -1;
+                        }
                     }
-                    if (stringCounter == 2)
+                    else if (command == "errs")
                     {
-                        Console.WriteLine(port + " " + link + " " + descr + " " + kv);
-                        portDescriptionList.Add(new PortDescription() { port = port.Trim(), link = link.Trim(), descr = descr.Trim(), kv = kv });
-                        stringCounter = -1;
+                        if (line.Contains("Port Number")) stringCounter = 0;
+                        if (stringCounter >= 0)
+                        {
+                            if (stringCounter == 0)
+                            {
+                                port = line.Substring(16, line.Length - 16);
+                            }
+                            else if (stringCounter == 6)
+                            {
+                                if (isDGS)
+                                {
+                                    errCRCRX = line.Substring(21, 10).ToLower();
+                                }
+                                else
+                                {
+                                    errCRCRX = line.Substring(17, 10).ToLower();
+                                }
+                            }
+                            else if (stringCounter == 8)
+                            {
+                                if (isDGS)
+                                {
+                                    errCRCTX = line.Substring(64, 10).ToLower();
+                                }
+                                else
+                                {
+                                    errCRCTX = line.Substring(60, 10).ToLower();
+                                }
+                            }
+                            if (stringCounter == 8)
+                            {
+                                Console.WriteLine(port + " " + errCRCRX + " " + errCRCTX);
+                                portErrorList.Add(new PortError() { port = port.Trim(), errCRCRX = errCRCRX.Trim(), errCRCTX = errCRCTX.Trim() });
+                                stringCounter = -1;
+                            }
+                        }
                     }
                 }
             }
 
             public string getJSONResult()
             {
-                return JsonConvert.SerializeObject(portDescriptionList, Formatting.Indented);
+                if (command == "desc")
+                {
+                    return JsonConvert.SerializeObject(portDescriptionList, Formatting.Indented);
+                }
+                else if (command == "errs")
+                {
+                    return JsonConvert.SerializeObject(portErrorList, Formatting.Indented);
+                }
+                return "";
             }
         }
 
@@ -163,6 +232,13 @@ namespace D_Link_description_console
             public string link { get; set; }
             public string descr { get; set; }
             public string kv { get; set; }
+        }
+
+        public class PortError
+        {
+            public string port { get; set; }
+            public string errCRCRX { get; set; }
+            public string errCRCTX { get; set; }
         }
     }
 }
